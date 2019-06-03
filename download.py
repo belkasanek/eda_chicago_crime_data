a6b3d6dee70182487fc4137d122f46d636153821
sasha <bogomazov95@gmail.com>
2019-06-02 22:13:26 -0700

    Update the download.py script. Added census data for 2010.

--- download.py
+++ download.py
@@ -9,6 +9,10 @@ try:
     url = 'https://data.cityofchicago.org/api/views/z8bn-74gv/rows.csv?accessType=DOWNLOAD'  
     urllib.request.urlretrieve(url, './data/Police_Stations.csv')
     
+    # census data about community areas for 2010
+    url = 'https://datahub.cmap.illinois.gov/dataset/5700ba1a-b173-4391-a26e-48b198e830c8/resource/b30b47bf-bb0d-46b6-853b-47270fb7f626/download/CCASF12010CMAP.xlsx'
+    urllib.request.urlretrieve(url, './data/census_for_community_ares_2010.xlsx')
+    
     # geojson of Chicago Police Districts
     url = 'https://data.cityofchicago.org/api/geospatial/fthy-xz3r?method=export&format=GeoJSON'
     urllib.request.urlretrieve(url, './data/Boundaries_Police_Districts')
